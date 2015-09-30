407d602547766e426ce314f9a8aaf61f2bcbeb92
Eddie Woodley <ecwoodley@gmail.com>
2015-09-30 22:17:38 +0100

    feat(git): add my git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,14 +6,65 @@ then
   alias git=$hub_path
 fi
 
-# The rest of my fun git aliases
-alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
+# status
+alias gst='git status -sb'
+
+# commit
+alias gad='git add'
+alias gaa='git add -A'
+alias gcm='git commit'
+
+# tagging
+alias gtg='git tag'
+alias gta='git tag -a'
+
+# log
+alias glg="git log --graph --pretty=format:'%C(auto)%h -%d %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias gla="git log --graph --pretty=format:'%C(auto)%h -%d %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --all"
+
+# reset
+alias gre='git reset'
+alias grh='git reset --hard'
+alias gun='git reset --soft HEAD~1'
+
+# merging
+alias gmg='git merge --no-ff'
+alias gmgff='git merge --ff-only'
+
+# rebasing
+alias grb='git rebase'
+alias grc='git rebase --continue'
+alias grs='git rebase --skip'
+alias gra='git rebase --abort'
+
+# branch management
 alias gco='git checkout'
-alias gcb='git copy-branch-name'
-alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gcb='git checkout -b'
+alias gbd='git branch -d'
+alias gbD='git branch -D'
+
+# stash
+alias gsh='git stash'
+alias gsp='git stash pop'
+alias gsa='git stash apply'
+alias gsc='git stash clear'
+
+# diff
+alias gdi='git diff'
+alias gds='git diff --staged'
+
+# dealing with remotes
+alias gpu='git push'
+alias gpl='git pull --rebase'
+alias gsm='git smart-pull' # requires git-smart gem
+
+# bisect
+alias gbi='git bisect start'
+alias gbg='git bisect good'
+alias gbb='git bisect bad'
+alias gbs='git bisect skip'
+alias gbr='git bisect reset'
+
+# cleanup
+alias ggc='git gc'
+alias gcr='git remote prune origin'
