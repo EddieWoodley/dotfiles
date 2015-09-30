353e81160e84b3374181d41e8473c03d19f6f777
Eddie Woodley <ecwoodley@gmail.com>
2015-09-30 22:44:01 +0100

    feat(zsh): show last exit status on prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -46,32 +46,13 @@ need_push () {
   fi
 }
 
-ruby_version() {
-  if (( $+commands[rbenv] ))
-  then
-    echo "$(rbenv version | awk '{print $1}')"
-  fi
-
-  if (( $+commands[rvm-prompt] ))
-  then
-    echo "$(rvm-prompt | awk '{print $1}')"
-  fi
-}
-
-rb_prompt() {
-  if ! [[ -z "$(ruby_version)" ]]
-  then
-    echo "%{$fg_bold[yellow]%}$(ruby_version)%{$reset_color%} "
-  else
-    echo ""
-  fi
-}
-
 directory_name() {
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
+local ret_status="%(?:%{$fg_bold[green]%}↳ :%{$fg_bold[red]%}↳ %s)"
+
+export PROMPT=$'\n$(directory_name) $(git_dirty)$(need_push)\n${ret_status} '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
